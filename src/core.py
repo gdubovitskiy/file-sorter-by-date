289beab8e9ca2382587b4a1e99bb4f96d6899acf
gdubovitskiy <gdubovitskiy@ya.ru>
2025-07-31 01:31:09 +0300

    refactor clean_name

--- src/core.py
+++ src/core.py
@@ -57,7 +57,15 @@ def parse_date_from_filename(filename: str) -> Optional[datetime]:
     from src.config import DATE_FORMATS
 
     base_name = Path(filename).stem
-    clean_name = base_name.replace(" ", "").replace("-", "").replace("_", "").replace(".", "")
+    # fmt: off
+    clean_name = (
+        base_name
+        .replace(" ", "")
+        .replace("-", "")
+        .replace("_", "")
+        .replace(".", "")
+    )
+    # fmt: on
 
     for fmt in ["%Y%m%d%H%M%S", "%Y%m%d"]:
         try:
